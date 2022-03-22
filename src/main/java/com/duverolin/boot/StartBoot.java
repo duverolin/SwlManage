dcacd3e429eaed2a903274d1a2cfed397d9b0886
duverolin <duverolin@163.com>
2022-03-22 17:08:09 +0800

    Changes

--- src/main/java/com/duverolin/boot/StartBoot.java
+++ src/main/java/com/duverolin/boot/StartBoot.java
@@ -6,17 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-// 开启事务管理
-@SuppressWarnings("ALL")
-@EnableTransactionManagement
+
 //程序入口
 @SpringBootApplication
+// 开启事务管理
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.duverolin.mapper"})
 //扫描过滤器
 @ServletComponentScan(basePackages = "com.duverolin.filter")
 // 被纳入扫描的层
-@ComponentScan(basePackages = {"com.duverolin.controller", "com.duverolin.entity", "com.duverolin.services"})
-//扫描Mapper
-@MapperScan(basePackages = "com.duverolin.mapper")
+@ComponentScan(basePackages = {"com.duverolin.controller","com.duverolin.entity", "com.duverolin.services"})
 class StartBoot {
     /**
      * 用于启动SpringBoot
